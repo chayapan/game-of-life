88ab45d83fb932c1f661f3a92f693da91bb6b83e
Chayapan Khannabha <chayapan@gmail.com>
2024-04-27 09:07:00 +0700

    initial board

---
+++ README.md
@@ -0,0 +1,33 @@
+# An Implementation of Conway Game of Life
+
+
+## Javascript/Typescript
+
+[conway-jsx](conway-jsx/)
+
+
+## Python
+
+[conway-tkinter](conway-tkinter/)
+
+
+Notes:
+
+organize with separator
+https://tkdocs.com/tutorial/complex.html
+
+Menu bar: https://tkdocs.com/tutorial/menus.html
+
+event loop and progress bar
+https://tkdocs.com/tutorial/eventloop.html
+
+canvas reference https://www.geeksforgeeks.org/python-tkinter-canvas-widget/
+
+
+binding to cell https://python-course.eu/tkinter/events-and-binds-in-tkinter.php
+
+
+grid size:
+
+16x16 ok
+256x256 too big
---
+++ conway-tkinter/main.py
@@ -0,0 +1,74 @@
+import argparse
+import tkinter as tk
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--grid", "-g", type=str, choices=['4x4', '16x16',  '16x32', 
+                                                       '32x32', '32x64', '64x64', 
+                                                       '16x64', '16x96', '16x128', 
+                                                       '128x128', '256x256'],
+                    help="defind grid size. < 256x256")
+
+default_grid_size = (16,16)
+cells = {}
+state = {} # track state of a cell
+
+def receive_interaction(event):
+   # Co-ordinates.
+    x1, y1, x2, y2 = ( event.x - 3 ),( event.y - 3 ), ( event.x + 3 ),( event.y + 3 ) 
+    print(x1,y1,x2,y2)
+
+def make_cell(root, grid_row, grid_col):
+    cell = tk.Canvas(master=root, width=16, height=16, background='gray75')
+    cell.create_rectangle(0,0,16,16, fill='red') # init color
+    # cell.bind( "<B1-Motion>", receive_interaction )
+    cell.bind("<Button>",  lambda e: toggle_cell(grid_row, grid_col))
+    # cell.bind("<Return>",  lambda : toggle_cell(grid_row, grid_col))
+    cell.grid(row=grid_row, column=grid_col, sticky=(tk.N, tk.W, tk.E, tk.S))
+    cells[(grid_row, grid_col)] = cell # Set global reference
+    state[(grid_row, grid_col)] = 0 # Mark dead initally
+
+    return cell
+
+def toggle_cell(grid_row, grid_col):
+    print(f"Toggle row={grid_row} col={grid_col}")
+    # get state 
+    cell_state = state[(grid_row, grid_col)]
+    cell = cells[(grid_row, grid_col)]
+    # cell.destroy() 
+    if cell_state == 0:  # Dead -> Alive
+        cell.create_rectangle(0,0,16,16, fill='green')
+        state[(grid_row, grid_col)] = 1
+    if cell_state == 1:  # Alive -> Alive
+        cell.create_rectangle(0,0,16,16, fill='black')
+        state[(grid_row, grid_col)] = 0
+    # and so on
+
+def main(args):
+    if args.grid:
+        print(f"Grid specify to: {args.grid}")
+        p = args.grid.split('x')
+        nrow, ncol = int(p[0]), int(p[1])
+    else:
+        print(f"Grid : {default_grid_size}")
+        nrow, ncol = default_grid_size
+    window = tk.Tk()
+    frame1 = tk.Frame(master=window, width=100, height=100, bg="red")
+    frame1.pack()
+    for i in range(nrow):
+        for j in range(ncol):
+            b = make_cell(frame1, grid_row=i, grid_col=j)
+    my_platform = window.tk.call('tk', 'windowingsystem')
+    print(my_platform)
+    # Menu
+    window.option_add('*tearOff', tk.FALSE)
+    menubar = tk.Menu(window)
+    window['menu'] = menubar
+    menu_file = tk.Menu(menubar)
+    menu_edit = tk.Menu(menubar)
+    menubar.add_cascade(menu=menu_file, label='File')
+    menubar.add_cascade(menu=menu_edit, label='Edit')
+    window.mainloop()
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
